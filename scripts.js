d9f01c280eacde334a0b52cd05214d25a5d2d50c
emma-rva <amn_empress@yahoo.com>
2019-10-14 15:20:27 -0400

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,7 +1,7 @@
 const app = document.getElementById('root');
 
-const logo = document.createElement('img');
-logo.src = 'images/create-octocat.png';
+// const logo = document.createElement('img');
+// logo.src = 'images/create-octocat.png';
 
 const container = document.createElement('div');
 container.setAttribute('class', 'container');
