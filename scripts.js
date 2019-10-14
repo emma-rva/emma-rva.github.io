1fb5ba5c94018ff9a49a5e50a2ebfada5959a134
emma-rva <amn_empress@yahoo.com>
2019-10-14 16:16:05 -0400

    Initial

--- scripts.js
+++ scripts.js
@@ -1,10 +1,11 @@
 const app = document.getElementById('root');
 
 const logo = document.createElement('img');
-logo.src = 'images/sample.png';
+logo.src = 'logo.png';
 
 const container = document.createElement('div');
 container.setAttribute('class', 'container');
+
 app.appendChild(logo);
 app.appendChild(container);
 
