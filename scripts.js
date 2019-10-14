8c9163cfb4c7d3c4fbd3733f7fe1cabe290678aa
emma-rva <amn_empress@yahoo.com>
2019-10-14 16:05:22 -0400

    image upload

--- scripts.js
+++ scripts.js
@@ -1,7 +1,7 @@
 const app = document.getElementById('root');
 
 const logo = document.createElement('img');
-logo.src = 'images/create-octocat.png';
+logo.src = 'images/IMG_4326.jpg';
 
 const container = document.createElement('div');
 container.setAttribute('class', 'container');
