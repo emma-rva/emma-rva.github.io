cc6ac98af7222e9852cc85113a600e26016710fe
emma-rva <amn_empress@yahoo.com>
2019-10-14 15:31:51 -0400

    Initial Commit

--- scripts.js
+++ scripts.js
@@ -7,7 +7,7 @@ const container = document.createElement('div');
 container.setAttribute('class', 'container');
 
 // app.appendChild(logo);
-app.appendChild(container);
+// app.appendChild(container);
 
 var request = new XMLHttpRequest();
 request.open('GET', 'http://jservice.io/api/clues?value=200&category=10', true);
@@ -15,7 +15,6 @@ request.onload = function () {
 
   // Begin accessing JSON data here
   var data = JSON.parse(this.response);
-  if (request.status >= 0 && request.status < 4000) {
     data.forEach(clue => {
       const card = document.createElement('div');
       card.setAttribute('class', 'card');
@@ -31,11 +30,6 @@ request.onload = function () {
       card.appendChild(h1);
       card.appendChild(p);
     });
-  } else {
-    const errorMessage = document.createElement('marquee');
-    errorMessage.textContent = `Gah, it's not working!`;
-    app.appendChild(errorMessage);
-  }
 }
 
 request.send();
