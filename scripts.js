3b1c93ab2b8289b67d38fad48388b5a8a8593438
emma-rva <amn_empress@yahoo.com>
2019-10-14 16:21:37 -0400

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,7 +1,7 @@
 const app = document.getElementById('root');
 
 const logo = document.createElement('img');
-logo.src = 'images/create-octocat.png';
+logo.src = 'images/sample.png';
 
 const container = document.createElement('div');
 container.setAttribute('class', 'container');
@@ -10,7 +10,7 @@ app.appendChild(logo);
 app.appendChild(container);
 
 var request = new XMLHttpRequest();
-request.open('GET', 'https://ghibliapi.herokuapp.com/films', true);
+request.open('GET', 'http://jservice.io/api/clues?value=200&category=10', true);
 request.onload = function () {
 
   // Begin accessing JSON data here
@@ -21,11 +21,11 @@ request.onload = function () {
       card.setAttribute('class', 'card');
 
       const h1 = document.createElement('h1');
-      h1.textContent = movie.title;
+      h1.textContent = movie.answer;
 
       const p = document.createElement('p');
-      movie.description = movie.description.substring(0, 300);
-      p.textContent = `${movie.description}...`;
+      movie.question = movie.question.substring(0, 300);
+      p.textContent = `${movie.question}...`;
 
       container.appendChild(card);
       card.appendChild(h1);
