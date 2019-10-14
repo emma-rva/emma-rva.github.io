f27629fab75c0b3bc6ea042a8b89eaf49b48c1ad
emma-rva <amn_empress@yahoo.com>
2019-10-14 13:38:38 -0400

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -16,16 +16,16 @@ request.onload = function () {
   // Begin accessing JSON data here
   var data = JSON.parse(this.response);
   if (request.status >= 200 && request.status < 400) {
-    data.forEach(movie => {
+    data.forEach(clue => {
       const card = document.createElement('div');
       card.setAttribute('class', 'card');
 
       const h1 = document.createElement('h1');
-      h1.textContent = movie.title;
+      h1.textContent = clue.value;
 
       const p = document.createElement('p');
-      movie.description = movie.description.substring(0, 300);
-      p.textContent = `${movie.description}...`;
+      clue.description = clue.description.substring(0, 300);
+      p.textContent = `${clue.description}...`;
 
       container.appendChild(card);
       card.appendChild(h1);
