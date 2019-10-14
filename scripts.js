505b8e529e3783479dd90b55185e4347ce334ad7
emma-rva <amn_empress@yahoo.com>
2019-10-14 16:12:50 -0400

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -5,7 +5,6 @@ logo.src = 'images/sample.png';
 
 const container = document.createElement('div');
 container.setAttribute('class', 'container');
-
 app.appendChild(logo);
 app.appendChild(container);
 
@@ -15,21 +14,27 @@ request.onload = function () {
 
   // Begin accessing JSON data here
   var data = JSON.parse(this.response);
-    data.forEach(clue => {
+  if (request.status >= 200 && request.status < 400) {
+    data.forEach(movie => {
       const card = document.createElement('div');
       card.setAttribute('class', 'card');
 
       const h1 = document.createElement('h1');
-      h1.textContent = clue.title;
+      h1.textContent = movie.title;
 
       const p = document.createElement('p');
-      clue.description = clue.description.substring(0, 300);
-      p.textContent = `${clue.description}...`;
+      movie.description = movie.description.substring(0, 300);
+      p.textContent = `${movie.description}...`;
 
       container.appendChild(card);
       card.appendChild(h1);
       card.appendChild(p);
     });
+  } else {
+    const errorMessage = document.createElement('marquee');
+    errorMessage.textContent = `Gah, it's not working!`;
+    app.appendChild(errorMessage);
+  }
 }
 
 request.send();
