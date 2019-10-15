760f43f64e4e7aca93b0f128c49f2f03bbe165b2
emma-rva <amn_empress@yahoo.com>
2019-10-14 20:31:37 -0400

    Update script.js

--- script.js
+++ script.js
@@ -3,8 +3,8 @@
 //const logo = document.createElement('img');
 //logo.src = 'images/sample.png';
 
-//const container = document.createElement('div');
-//container.setAttribute('class', 'container');
+const container = document.createElement('div');
+container.setAttribute('class', 'container');
 
 //app.appendChild(logo);
 //app.appendChild(container);
